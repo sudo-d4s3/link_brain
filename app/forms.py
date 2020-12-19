036984738cad46b2f2b988a721e28f7a3e1a14a8
d4s3 <honeycutt.chandler@pm.me>
2020-12-19 00:49:47 -0500

    frontend data entry built
    there are now some front end forms with exception handling to add
    entries to the database

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Bookmarks
+
+class BookmarkForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    link = StringField('Link', validators=[DataRequired()])
+    submit = SubmitField('Save')
+    
+    def validate_title(self, title):
+        t = Bookmarks.query.filter_by(title=title.data).first()
+        if t is not None:
+            raise ValidationError("You already have this")
+
+    def validate_link(self, link):
+        l = Bookmarks.query.filter_by(link=link.data).first()
+        if l is not None:
+            raise ValidationError("You already have this")
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash
+from app import app, db
 from app.models import Bookmarks
+from app.forms import BookmarkForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     bookmarks = Bookmarks.query.all()
-    return render_template('index.html', bookmarks=bookmarks)
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        bookmark = Bookmarks(title=form.title.data, link=form.link.data)
+        db.session.add(bookmark)
+        db.session.commit()
+        flash('Bookmark Saved!')
+    return render_template('index.html', bookmarks=bookmarks, form=form)
