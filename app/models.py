a8424ae5c773a3d07fc88ef56d61c1b7c8bea6e5
d4s3 <honeycutt.chandler@pm.me>
2020-12-20 15:44:05 -0500

    tagging system implemented
    a many-to-many relationship has been created, there are now 3 tables:
    Bookmark (the main table), Tag (where the the tags are), Tags (the
    helper table)
    redo how it renders in the frontend

--- app/models.py
+++ app/models.py
@@ -1,10 +1,23 @@
 from app import db
 
+tags = db.Table('tags',
+        db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+        db.Column('boodmark_id', db.Integer, db.ForeignKey('bookmarks.id'), primary_key=True)
+)
+
 class Bookmarks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(16), index=True, unique=True)
     link = db.Column(db.String, index=True, unique=True)
-    tags = db.Column(db.String, index=True)
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+            backref=db.backref('bookmark', lazy=True))
 
     def __repr__(self):
         return '<Bookmarks {}>'.format(self.title)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, index=True, unique=True)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.name)
