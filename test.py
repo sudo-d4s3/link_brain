116dbf11bb5368f7ae7e765bbd09097a77437fc6
sudo-d4s3 <d4s3@d4s3.xyz>
2024-10-17 01:12:54 -0500

    edited for library use

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
 from ast import literal_eval
 
-content_text = """This is a test file.
-I'm the dumbass writing his own markup language.
-I know there is a markup language that is very similar to this but the internet sucks and everything is enshittified!"""
-markup_text = """(1,4)#:(1,5)+:(2,3)#:(2,4)+:(2,6)-:(2,7)*:(3,2)#:(3,8,11)-:(3,10)*"""
-markup_text2 = """(1,4)#:(1,5)+:(2,3)#:(2,4)+:(2,6)-:(2,7)*:(3,2)#:(3,10)*:(3,8,11)-"""
-markup_text3 = """(2,4)+:(1,4)#:(1,5)+:(2,3)#:(3,8,11)-:(2,6)-:(2,7)*:(3,2)#:(3,10)*"""
-
 
 def token_parser(markup):
     tmp_dict = {}
@@ -25,7 +18,7 @@ def markup_parser(markup):
 def markup_engine(content, markup):
     content = content.splitlines()
     for i, c in enumerate(content):
-        content[i] = c.split() 
+        content[i] = c.split()
     markup = markup_parser(markup)
     msg_join = ""
     for i, x in enumerate(markup):
@@ -33,25 +26,25 @@ def markup_engine(content, markup):
         value = list(x.values())[0]
 
         match value:
-            case '#':
+            case "#":
                 try:
                     if key[2]:
                         msg_join = f"<mark>{' '.join(content[key[0] - 1][key[1] - 1: key[2]])}</mark>"
                 except:
                     msg_join = f"<mark>{content[key[0] - 1][key[1] - 1]}</mark>"
-            case '-':
+            case "-":
                 try:
                     if key[2]:
                         msg_join = f"<u>{' '.join(content[key[0] - 1][key[1] - 1: key[2]])}</u>"
                 except:
                     msg_join = f"<u>{content[key[0] - 1][key[1] - 1]}</u>"
-            case '+':
+            case "+":
                 try:
                     if key[2]:
                         msg_join = f"<b>{' '.join(content[key[0] - 1][key[1] - 1: key[2]])}</b>"
                 except:
                     msg_join = f"<b>{content[key[0] - 1][key[1] - 1]}</b>"
-            case '*':
+            case "*":
                 try:
                     if key[2]:
                         msg_join = f"<i>{' '.join(content[key[0] - 1][key[1] - 1: key[2]])}</i>"
@@ -63,10 +56,9 @@ def markup_engine(content, markup):
                 line_msg_split = line_msg_join.split()
                 content[key[0] - 1] = line_msg_split
         except:
-            content[key[0] - 1][key[1]-1] = msg_join
-    for i,c in enumerate(content):
-        content[i] = ' '.join(content[i])
-        
+            content[key[0] - 1][key[1] - 1] = msg_join
+    for i, c in enumerate(content):
+        content[i] = " ".join(content[i])
 
     return "".join(
         [
@@ -77,10 +69,20 @@ def markup_engine(content, markup):
     )
 
 
-test_html =  """<!DOCTYPE html><html><head></head><body>This is a <mark>test</mark> <b>file.</b><br />I'm the <mark>dumbass</mark> <b>writing</b> his <u>own</u> <i>markup</i> language.<br />I <mark>know</mark> there is a markup language <u>that is <i>very</i> similar</u> to this but the internet sucks and everything is enshittified!</body></html>"""
-#markup_engine(content_text, markup_text)
-print(markup_engine(content_text, markup_text))
-
-assert markup_engine(content_text, markup_text) == test_html
-assert markup_engine(content_text, markup_text2) == test_html
-assert markup_engine(content_text, markup_text3) == test_html
+if __name__ == "__main__":
+    content_text = """This is a test file.
+    I'm the dumbass writing his own markup language.
+    I know there is a markup language that is very similar to this but the internet sucks and everything is enshittified!"""
+    test_html = """<!DOCTYPE html><html><head></head><body>This is a <mark>test</mark> <b>file.</b><br />I'm the <mark>dumbass</mark> <b>writing</b> his <u>own</u> <i>markup</i> language.<br />I <mark>know</mark> there is a markup language <u>that is <i>very</i> similar</u> to this but the internet sucks and everything is enshittified!</body></html>"""
+    markup_text = (
+        """(1,4)#:(1,5)+:(2,3)#:(2,4)+:(2,6)-:(2,7)*:(3,2)#:(3,8,11)-:(3,10)*"""
+    )
+    assert markup_engine(content_text, markup_text) == test_html
+    markup_text2 = (
+        """(1,4)#:(1,5)+:(2,3)#:(2,4)+:(2,6)-:(2,7)*:(3,2)#:(3,10)*:(3,8,11)-"""
+    )
+    assert markup_engine(content_text, markup_text2) == test_html
+    markup_text3 = (
+        """(2,4)+:(1,4)#:(1,5)+:(2,3)#:(3,8,11)-:(2,6)-:(2,7)*:(3,2)#:(3,10)*"""
+    )
+    assert markup_engine(content_text, markup_text3) == test_html
